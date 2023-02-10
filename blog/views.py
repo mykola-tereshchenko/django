0fd2146d2260a8aa860659b02ec797ee2801063e
mykola-tereshchenko <tereshchenko.nikolay@gmail.com>
2023-02-10 16:26:28 +0200

    Add support of static files

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
+
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'posts' : posts,
+        'title': 'Zen of Python'
+    }
+    return render(request, 'index.html', context)
 
 
 def about(request):
