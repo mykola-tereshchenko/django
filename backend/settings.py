6b6309bf896a7ec4666ba4f29dfd9cdadedc3b50
mykola-tereshchenko <tereshchenko.nikolay@gmail.com>
2023-02-10 14:36:31 +0200

    Configure settings of basic pages

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +60,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
