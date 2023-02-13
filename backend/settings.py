b5dbfd99471aa5b1c246506a0fe1f2f11d4a277f
mykola-tereshchenko <tereshchenko.nikolay@gmail.com>
2023-02-13 18:48:28 +0200

    Add login form

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'blog',
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
